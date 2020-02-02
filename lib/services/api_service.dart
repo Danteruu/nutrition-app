ebf681676433a57543057dc60b435f747b1df6c9
danteruu <filipchojna99@gmail.com>
2020-02-02 11:38:24 +0100

    Added functions to ApiService class Functions fetch json data and convert it to class objects

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,14 +1,72 @@
 import 'dart:convert';
 import 'dart:io';
 import 'package:nutrition/models/meal_plan_model.dart';
+import 'package:nutrition/models/recipe_model.dart';
 import 'package:http/http.dart' as http;
 
-class APIService{
-
+class APIService {
   APIService._instantiate();
 
   static final APIService instance = APIService._instantiate();
 
   final String _baseUrl = 'api.spoonacular.com';
-  static const String  API_KEY = 'c1b19c9e5872409e91be89ab00ce7e8c';
+  static const String API_KEY = 'c1b19c9e5872409e91be89ab00ce7e8c';
+
+  // Generate Meal Plan
+  Future<MealPlan> generateMealPlan({int targetCalories, String diet}) async {
+    if (diet == 'None') diet = '';
+
+    Map<String, String> parameters = {
+      'timeFrame': 'day',
+      'targetCalories': targetCalories.toString(),
+      'diet': diet,
+      'apiKey': API_KEY,
+    };
+
+    Uri uri = Uri.https(
+      _baseUrl,
+      'recipes/mealplans/generate',
+      parameters,
+    );
+
+    Map<String, String> headers = {
+      HttpHeaders.contentTypeHeader: 'application/json',
+    };
+
+    try {
+      var response = await http.get(uri, headers: headers);
+      Map<String, dynamic> data = json.decode(response.body);
+      MealPlan mealPlan = MealPlan.fromMap(data);
+      return mealPlan;
+    } catch (err) {
+      throw err.toString();
+    }
+  }
+
+  // Recipe Info
+  Future<Recipe> fetchRecipe(String id) async {
+    Map<String, String> parameters = {
+      'includeNutrition': 'false',
+      'apiKey': API_KEY,
+    };
+
+    Uri uri = Uri.https(
+      _baseUrl,
+      '/recipes/$id/information',
+      parameters,
+    );
+
+    Map<String, String> headers = {
+      HttpHeaders.contentTypeHeader: 'application/json',
+    };
+
+    try{
+      var response = await http.get(uri, headers: headers);
+      Map<String, dynamic> data = json.decode(response.body);
+      Recipe recipe = Recipe.fromMap(data);
+      return recipe;
+    } catch (err) {
+      throw err.toString();
+    }
+  }
 }
